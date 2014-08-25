94e76b4f40dc2fbc432023994a84e18077a1ebf2
Cristian Zaloj <czaloj@gmail.com>
2014-08-25 01:27:50 -0400

    F This, Making Batch Scripts
    Remeber To Use Them

---
+++ SoAPostCheckout.bat
@@ -0,0 +1,2 @@
+utils\xmljson.exe False SoAProject.json SoA/SoA.vcxproj
+utils\xmljson.exe False SoAFilters.json SoA/SoA.vcxproj.filters
---
+++ SoAPreCommit.bat
@@ -0,0 +1,2 @@
+utils\xmljson.exe True SoA/SoA.vcxproj SoAProject.json
+utils\xmljson.exe True SoA/SoA.vcxproj.filters SoAFilters.json
