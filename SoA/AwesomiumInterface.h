e0fb3899dd7a6e19d2bdfa8ff43c31cced3f8dae
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2014-10-02 12:35:40 -0500

    Added more headers...

--- SoA/AwesomiumInterface.h
+++ SoA/AwesomiumInterface.h
@@ -2,67 +2,18 @@
 #define _AWESOMIUM_INTERFACE_H_
 #pragma once
 
+#include <Awesomium/BitmapSurface.h>
+#include <Awesomium/DataPak.h>
+#include <Awesomium/DataSource.h>
+#include <Awesomium/STLHelpers.h>
+#include <Awesomium/WebCore.h>
 #include "Awesomium/WebKeyboardCodes.h"
 #include <SDL/SDL_keycode.h>
 
 
-class CustomJSMethodHandler : public Awesomium::JSMethodHandler
-{
-public:
-    void OnMethodCall(Awesomium::WebView *caller, unsigned int remote_object_id, const Awesomium::WebString &method_name, const Awesomium::JSArray &args);
-    Awesomium::JSValue OnMethodCallWithReturnValue(Awesomium::WebView *caller, unsigned int remote_object_id, const Awesomium::WebString &method_name, const Awesomium::JSArray &args);
-
-    Awesomium::JSObject *myObject;
-};
-
-class AwesomiumInterface {
-public:
-    AwesomiumInterface();
-    ~AwesomiumInterface();
-
-    bool init(const char *inputDir, int width, int height);
-
-    void update();
-    void draw();
-
-    void setDrawCoords(int x, int y, int width, int height);
-    void setColor(i32v4 color);
-
-private:
-    bool _isInitialized;
-
-    int _width, _height;
-
-    GLuint _renderedTexture;
-
-    GLuint _vboID;
-    GLuint _elementBufferID;
-
-    ui8 _color[4];
-
-    unsigned short _numFiles;
-
-    Awesomium::DataSource* _data_source;
-    Awesomium::WebSession* _webSession;
-    Awesomium::WebCore* _webCore;
-    Awesomium::WebView* _webView;
-
-    Awesomium::JSValue _jsValue;
-
-    CustomJSMethodHandler _methodHandler;
-
-    struct Vertex2D {
-        f32v2 pos;
-        ui8 uv[2];
-        ui8 pad[2];
-        ui8 color[4];
-    };
-};
-
 #define mapWebKey(a, b) case Key_##a: return Awesomium::KeyCodes::AK_##b;
 
-enum Key
-{
+enum Key {
     Key_Unknown = SDL_SCANCODE_UNKNOWN,
 
     Key_A = SDL_SCANCODE_A,
@@ -396,12 +347,62 @@ enum Key
     Key_Alt
 };
 
-enum KeyScanCode
-{
-
-};
 
 int getWebKeyFromKey(Key key);
 
+
+class CustomJSMethodHandler : public Awesomium::JSMethodHandler
+{
+public:
+    void OnMethodCall(Awesomium::WebView *caller, unsigned int remote_object_id, const Awesomium::WebString &method_name, const Awesomium::JSArray &args);
+    Awesomium::JSValue OnMethodCallWithReturnValue(Awesomium::WebView *caller, unsigned int remote_object_id, const Awesomium::WebString &method_name, const Awesomium::JSArray &args);
+
+    Awesomium::JSObject *myObject;
+};
+
+
+class AwesomiumInterface {
+public:
+    AwesomiumInterface();
+    ~AwesomiumInterface();
+
+    bool init(const char *inputDir, int width, int height);
+
+    void update();
+    void draw();
+
+    void setDrawCoords(int x, int y, int width, int height);
+    void setColor(i32v4 color);
+
+private:
+    bool _isInitialized;
+
+    int _width, _height;
+
+    GLuint _renderedTexture;
+
+    GLuint _vboID;
+    GLuint _elementBufferID;
+
+    ui8 _color[4];
+
+    unsigned short _numFiles;
+
+    Awesomium::DataSource* _data_source;
+    Awesomium::WebSession* _webSession;
+    Awesomium::WebCore* _webCore;
+    Awesomium::WebView* _webView;
+
+    Awesomium::JSValue _jsValue;
+
+    CustomJSMethodHandler _methodHandler;
+
+    struct Vertex2D {
+        f32v2 pos;
+        ui8 uv[2];
+        ui8 pad[2];
+        ui8 color[4];
+    };
+};
 
 #endif  // _AWESOMIUM_INTERFACE_H_
